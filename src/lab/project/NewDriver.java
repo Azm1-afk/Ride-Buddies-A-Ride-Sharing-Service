c39afb235e34d937ea8e29ddc9302fce00e07434
Mashrafi Ibn Reja <rangdhanu322@gmail.com>
2023-06-16 16:05:18 +0600

    Source

---
+++ src/lab/project/NewDriver.java
@@ -0,0 +1,11 @@
+package lab.project;
+
+public class NewDriver {
+    private String driver_rname;
+    private String driver_password;
+
+    public NewDriver(String a, String b) {
+        driver_rname = a;
+        driver_password = b;
+    }    
+}
