474b374f0c011ce98545c956ab27f6aca6ddead1
Mashrafi Ibn Reja <rangdhanu322@gmail.com>
2023-06-16 20:28:41 +0600

    .

--- src/lab/project/NewRiderWindow.java
+++ src/lab/project/NewRiderWindow.java
@@ -91,7 +91,7 @@ public class NewRiderWindow extends javax.swing.JFrame {
 
     private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
         String rider_name = jTextField1.getText();
-        rider.setRider_name(rider_name);        
+                
         System.out.println(rider_name);
         
         try{
